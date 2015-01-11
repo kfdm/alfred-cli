4281a1a014e06b885f257daea7aa57b0cdddd1c7
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-01-11 21:03:53 +0900

    Wrap our build command using a click.command()

--- alfredcli/cli.py
+++ alfredcli/cli.py
@@ -1,2 +1,34 @@
-def main():
-    return 0
+import os
+import subprocess
+import zipfile
+import logging
+
+import click
+
+
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+@click.option('-v', '--verbose', count=True)
+def main(verbose):
+    logging.basicConfig(level=(logging.WARNING - 10 * verbose))
+
+
+@main.command()
+def build():
+    project_root = os.path.realpath('.')
+    project_name = os.path.basename(project_root)
+    archive_path = os.path.join(project_root, project_name + '.alfredworkflow')
+
+    logger.info('Reading %s', project_root)
+    logger.info('Building %s', project_name)
+
+    with zipfile.ZipFile(archive_path, 'a') as zfp:
+        # git-ls-files will let us take advantage of .gitignore to ignore
+        # files that we do not want to save in our extension
+        for path in subprocess.check_output(['git', 'ls-files']).strip().split('\n'):
+            logger.info('Adding %s', path)
+            zfp.write(path, os.path.basename(path))
+
+    click.echo('Built workflow ' + archive_path)
